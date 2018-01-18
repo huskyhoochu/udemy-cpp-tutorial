f9b3997ed2bff0df83bca7f452921f6c8d0374cf
huskyhoochu <dfg1499@gmail.com>
2018-01-18 17:39:13 +0900

    pointer

--- pointers_and_arrays/main.cpp
+++ pointers_and_arrays/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::string texts[] = {"one", "two", "three"};
+
+//    std::string *pTexts = texts;
+
+    // range-based for loop
+    // Type inference by reference.
+    // Observes in-place. Preferred when no modify is needed.
+    for (const auto &y : texts) {
+        std::cout << y << " " << std::flush;
+    }
+
+//    for (int i = 0; i < sizeof(texts) / sizeof(std::string); i++) {
+//        std::cout << pTexts[i] << " " << std::flush;
+//    }
+
     return 0;
 }
