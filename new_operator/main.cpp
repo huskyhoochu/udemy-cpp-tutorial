7ed4e08092f9d208eb42b67cfa608b019a233b9f
huskyhoochu <dfg1499@gmail.com>
2018-01-22 17:37:08 +0900

    returning object from function practice

--- new_operator/main.cpp
+++ new_operator/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "Animal.h"
 
+Animal *createAnimal(std::string name) {
+    auto a = new Animal();
+    a -> setName(name);
+    return a;
+}
+
 int main() {
 
     auto pCat1 = new Animal();
     pCat1 -> setName("Husky");
     (*pCat1).speak();
     delete pCat1;
-    
+
+    auto frog = createAnimal("Mike");
+    frog -> speak();
+    delete frog;
+
     return 0;
 }
