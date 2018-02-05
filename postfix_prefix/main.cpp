5e4e1ace2b4a30ea286760626eef1c085004aefa
huskyhoochu <dfg1499@gmail.com>
2018-02-05 18:48:11 +0900

    postfix practice update

---
+++ postfix_prefix/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    int value = 8;
+
+    std::cout << value << std::endl;
+
+    int number = ++value;
+
+    std::cout << number << std::endl;
+
+    return 0;
+}
